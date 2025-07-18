69cb7b828b55120afaffc173ac500a579b77df29
Kiran Mulmi <kirn.mulmi@monotype.com>
2025-07-18 08:00:24 +0545

    Logout

--- src/components/backend/Header.js
+++ src/components/backend/Header.js
@@ -1,7 +1,18 @@
+import { useNavigate } from "react-router";
+
 const Header = () => {
+  const navigate = useNavigate();
+  const handleBtnClick = () => {
+    localStorage.removeItem('AUTH_TOKEN');
+    navigate('/admin/login');
+  }
+
   return (
     <div class="row">
-      <div class="header">Header</div>
+      <div class="header">
+        <h2>Header</h2>
+        <div><button onClick={() => handleBtnClick()}>Logout</button></div>
+      </div>
     </div>
   );
 }
